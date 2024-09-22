22ba151ed38dffa3561ab227d64dcf35a349ee73
macksih <mhbobeh.jamali@gmail.com>
2024-09-23 01:04:45 +0330

    modified dataDashboard page

--- iran-jack/src/app/dataDashboard/page.tsx
+++ iran-jack/src/app/dataDashboard/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import MobileView from '../../component/mobileView/MobileView';
+import DeskTopView from '../../component/desktopView/DeskTopView'
 
 function Page() {
     return(
-        <div className='container lg:mx-auto lg:px-16 px-3 justify-between  pb-3 bg-white rounded-[10px] leading-10 text-justify'>
+        <div className='container justify-between  pb-3 bg-white rounded-[10px] leading-10 text-justify'>
             <div className='lg:hidden flex'>
                 <MobileView />
+            </div>
+            <div className='lg:flex hidden'>
+                <DeskTopView />
             </div>
             
         </div>
